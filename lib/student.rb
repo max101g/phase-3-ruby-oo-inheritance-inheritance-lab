946f5d6d9923e0896d0cce965bdf3849836c636d
max101g <maxgithinji101@gmail.com>
2023-02-15 01:08:15 +0300

    Ruby commit

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,17 @@
-class Student 
+require_relative './user'
 
+class Student < User
+    attr_accessor :knowledge
+
+    def initialize
+        @knowledge = []
+    end
+
+    def learn(str_of_knowledge)
+        @knowledge << str_of_knowledge
+    end
+
+    def knowledge
+        @knowledge
+    end
 end
